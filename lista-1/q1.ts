ba9ba4917b460e18c813fbd17d4178ae559cfa76
Elzilane Barreto <elzilanebarreto@gmail.com>
2025-08-27 10:55:59 -0400

    Adicionando as bibliotecas necessárias e o enunciado da questão 1

---
+++ lista-1/q1.ts
@@ -0,0 +1,5 @@
+/* Collection Array (Resolva 1 Exercício)
+    Escreva um programa para criar uma Collection Array de Objetos do tipo string. O programa deverá solir ao usuário, que ele digite via teclado 5 cores e deverá adicioná-las individualmente no Array. Em seguida, faça o que se pede:
+        ● Mostre na tela todas as cores adicionadas.
+        ● Mostre na tela todas as cores adicionadas, ordenadas em ordem crescente.
+*/
